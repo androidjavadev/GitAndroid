6a2ac27e6b02c52d9a1884d68ffda9faf58ecdac
karthickmaestro <roughtest2@gmail.com>
2016-11-24 11:57:31 +0530

    commit committ

--- app/src/main/java/com/gitandroid/MainActivity.java
+++ app/src/main/java/com/gitandroid/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         String e="Welcome";
 
         Log.e("BB","BB");
+        Log.e("BB","BB");
+        Log.e("BB","BB");
+        Log.e("BB","BB");
 
     }
 }
